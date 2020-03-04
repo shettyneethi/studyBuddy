52a72c586492c4be530f33cf3c3d489a88e033b8
Neethi <neethi.shetty@colorado.edu>
2020-03-03 20:46:39 -0700

    updating welcome msg

--- Web/src/login.jsx
+++ Web/src/login.jsx
@@ -6,33 +6,27 @@ class Login extends React.Component {
     super();
 
     this.state = {
+        usrName : "",
         isLoggedIn : false,
         message : ""
     }
 
   }
-  handleClick1 = () => {
-  if(this.state.class1 == "available"){
-    this.setState({
-        class1: "selected",
-        start_time : "5:00 AM"
-      });
-      }else{
-      this.setState({
-        class1: "available"
-      });
-      }
-  
+
+
+  handleChange = (evt) => {
+        this.setState({ usrName: evt.target.value });
   }
+
+
+
   handleSubmit= () => {
   this.setState({
         isLoggedIn : true,
-        message : "WELCOME TO STUDY BUDDY!!!"
+        message : "Welcome to StudyBuddy " + this.state.usrName + "!!!"
       });
   
   }
-
-
 
   render() {
 
@@ -46,7 +40,7 @@ class Login extends React.Component {
              <form class="loginForm">
                 <h1 align="center">LOGIN</h1>
 
-                <input type="text" name="username" placeholder="Username" required></input><br/><br/>
+                <input type="text" name="username" onChange={this.handleChange} value={this.state.usrName} placeholder="Username" required></input><br/><br/>
                 <input type="password" name="password" placeholder="Password" required></input><br/><br/>
                 <button type="button" onClick={this.handleSubmit}>Submit</button>
             </form>
