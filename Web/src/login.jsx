ac05ad9705f92bde5f132100f17141f4a5289873
gitshanks <nishankatwork@gmail.com>
2020-03-12 22:11:35 -0600

    made some Login UI changes

--- Web/src/login.jsx
+++ Web/src/login.jsx
@@ -1,52 +1,53 @@
 import React from 'react';
 import './login.css'
-
+import { Button, TextField } from '@material-ui/core';
 class Login extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-        usrName : "",
-        isLoggedIn : false,
-        message : ""
+      usrName: "",
+      isLoggedIn: false,
+      message: ""
     }
 
   }
 
 
   handleChange = (evt) => {
-        this.setState({ usrName: evt.target.value });
+    this.setState({ usrName: evt.target.value });
   }
 
 
 
-  handleSubmit= () => {
-  this.setState({
-        isLoggedIn : true,
-        message : "Welcome to StudyBuddy " + this.state.usrName + "!!!"
-      });
-  
+  handleSubmit = () => {
+    this.setState({
+      isLoggedIn: true,
+      message: "Welcome to StudyBuddy " + this.state.usrName + "!!!"
+    });
+
   }
 
   render() {
 
-    return(
-    <div>
-        {this.state.isLoggedIn ? <h1 align="center"> {this.state.message}</h1> : null }
- 
+    return (
+      <div>
+        {this.state.isLoggedIn ? <h1 align="center"> {this.state.message}</h1> : null}
+
         {this.state.isLoggedIn ? null :
           <div id="login-form" align="center">
 
-             <form class="loginForm">
-                <h1 align="center">LOGIN</h1>
+            <form class="loginForm">
+              <h1 align="center">LOGIN</h1>
 
-                <input type="text" name="username" onChange={this.handleChange} value={this.state.usrName} placeholder="Username" required></input><br/><br/>
-                <input type="password" name="password" placeholder="Password" required></input><br/><br/>
-                <button type="button" onClick={this.handleSubmit}>Submit</button>
+              <TextField required id="standard-required" type="text" label="Username" name="username" onChange={this.handleChange} value={this.state.usrName} ></TextField><br /><br />
+
+              <TextField required id="standard-required" type="password" name="password" label="Password" ></TextField ><br /><br />
+              <Button variant="outlined" color="primary" onClick={this.handleSubmit}>Submit</Button>
             </form>
           </div>
         }
-    </div>
+      </div>
 
     )
   }
