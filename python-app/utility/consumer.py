880d61157ececa1d1d63857c18de5887144537ff
Yash Sapra <yasa0041@colorado.edu>
2020-04-04 14:45:18 -0600

    Change Consumer to return a consumer

--- python-app/utility/consumer.py
+++ python-app/utility/consumer.py
@@ -8,10 +8,33 @@ TOPIC_NAME = 'posts'
 consumer = KafkaConsumer(TOPIC_NAME,
                          bootstrap_servers=KAFKA_IP, 
                          group_id=None,
-                         auto_offset_reset='earliest',                         
+                         auto_offset_reset='latest',
+                         enable_auto_commit=False
                          )
 
+def getConsumer(readLatest=False):
+    if (readLatest):
+         return KafkaConsumer(TOPIC_NAME,
+                         bootstrap_servers=KAFKA_IP, 
+                         group_id=None,
+                         auto_offset_reset='latest',
+                         enable_auto_commit=False
+                         )
+    else:
+        return KafkaConsumer(TOPIC_NAME,
+                         bootstrap_servers=KAFKA_IP, 
+                         group_id=None
+                        )
 
-for message in consumer:
-    if message is not None:
-        print (loads(message.value.decode('ascii')))
+
+if __name__ == '__main__':
+    consumer = KafkaConsumer(TOPIC_NAME,
+                         bootstrap_servers=KAFKA_IP, 
+                         group_id=None,
+                         auto_offset_reset='latest',
+                         enable_auto_commit=False
+                         )
+
+    for message in consumer:
+        if message is not None:
+            print (loads(message.value.decode('ascii')))
