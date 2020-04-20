8f43bb4710aa9b9923602dda926f4712c19af88c
Neethi <neethi.shetty@colorado.edu>
2020-04-20 15:15:33 -0600

    fixings unit tests

--- python-app/test/test_login.py
+++ python-app/test/test_login.py
@@ -5,11 +5,11 @@ import sys
 from flask import Flask
 from unittest.mock import Mock, patch
 sys.path.append("..")
-import login
+import main
 
 class LoginAPITest(unittest.TestCase):
   def setUp(self):
-    self.client = login.app.test_client
+    self.client = main.app.test_client
     self.login_request_success = {
         "user_name" : "admin",
         "password" : "password"
@@ -29,7 +29,7 @@ class LoginAPITest(unittest.TestCase):
         "email" : "admin@gmail.com"
     }
 
-  @patch('login.pymongo.MongoClient')
+  @patch('main.pymongo.MongoClient')
   def  test_success_login(self, mock_client):
     mock_client()["STUDYBUDDY"]["user_details"].find(self.login_request_success).count.return_value = 1
     # print("here"+str(mock_client()["STUDYBUDDY"]["user_details"].find(self.login_request_success)))
@@ -38,7 +38,7 @@ class LoginAPITest(unittest.TestCase):
     self.assertEqual(response.status, '200 OK')
     self.assertIn( b'"status": "SUCCESS"',response.data)
 
-  @patch('login.pymongo.MongoClient')
+  @patch('main.pymongo.MongoClient')
   def  test_fail_login(self, mock_client):
       mock_client()["STUDYBUDDY"]["user_details"].find(self.login_request_fail).count.return_value = 0
       # print("here"+str(mock_client["STUDYBUDDY"]["user_details"].find(self.login_request_fail)))
@@ -47,7 +47,7 @@ class LoginAPITest(unittest.TestCase):
       self.assertEqual(response.status, '200 OK')
       self.assertIn( b'"status": "FAIL"',response.data)
 
-  @patch('login.pymongo.MongoClient')
+  @patch('main.pymongo.MongoClient')
   def  test_success_signup(self, mock_client):
         mock_client()["STUDYBUDDY"]["user_details"].find({ "user_name": "guest"}).count.return_value = 0
         mock_client()["STUDYBUDDY"]["user_details"].find({ "email": "guest@gmail.com"}).count.return_value = 0
@@ -55,12 +55,12 @@ class LoginAPITest(unittest.TestCase):
         response = self.client().post('/api/signup', headers={'Content-Type': 'application/json'}, data=json.dumps(self.signup_request_success))
         # print(response)
         self.assertEqual(response.status, '200 OK')
-        self.assertIn(b'"message": "Sign Up Successful", "status": "SUCCESS"', response.data)
+        self.assertIn(b'"message": "Sign Up Success!", "status": "SUCCESS"', response.data)
 
-  @patch('login.pymongo.MongoClient')
+  @patch('main.pymongo.MongoClient')
   def  test_fail_signup(self, mock_client):
         mock_client()["STUDYBUDDY"]["user_details"].find({ "user_name": "guest"}).count.return_value = 1
-        response = self.client().post('/api/signup', headers={'Content-Type': 'application/json'}, data=json.dumps(self.signup_request_success))
+        response = self.client().post('/api/signup', headers={'Content-Type': 'application/json'}, data=json.dumps(self.signup_request_fail))
         # print(response)
         self.assertEqual(response.status, '200 OK')
         self.assertIn(b'"message": "Account with this username already exists", "status": "FAIL"', response.data)
