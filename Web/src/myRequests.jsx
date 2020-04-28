af835b92c02457e82c76aa53eae4495c07a79b8b
Reshma chowdary Morampudi <reshmachowdarymorampudi@Reshmas-Air.hsd1.co.comcast.net>
2020-04-27 22:33:43 -0600

    fix delete in MyRequests page

--- Web/src/myRequests.jsx
+++ Web/src/myRequests.jsx
@@ -12,6 +12,7 @@ import {
   } from 'react-router-dom';
 
 class MyRequests extends Component {
+    _isMounted = false;
     state = {
         posts: []
     };
@@ -31,7 +32,29 @@ class MyRequests extends Component {
             .then((data) => {
                 this.setState({posts: data});
         }); 
+        this._isMounted = true;
+    
+        this.eventSource= new EventSource('https://34.71.199.201:8081/api/deleted/posts');
+        this.eventSource.onmessage = e =>
+        this.deletePost(JSON.parse(e.data), e);
 
+    }
+
+    deletePost(data, e) {
+        console.log('In delete post')
+        let post = this.state.posts
+        let post_id = data['_id']
+        post = post.filter(item => item['_id']['$oid'] !== post_id)
+        console.log(post)
+    
+        this.setState({ posts: post});
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    
+        if (this.eventSource)
+          this.eventSource.close();
     }
 
   
